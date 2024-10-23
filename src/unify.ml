11f53fd35c0d5f41bd6008244baf2c813f2313a7
s-moyen <s.moyen0007@gmail.com>
2024-10-23 10:09:27 +0200

    Ajout de unify

---
+++ src/unify.ml
@@ -0,0 +1,25 @@
+exception Unification_failure
+
+let rec unify t1 t2 =
+  match t1, t2 with
+  |Term.Fun(f, sous_termes1), Term.Fun(g, sous_termes2) ->
+    if f=g then
+      try 
+        List.iter2 unify sous_termes1 sous_termes2
+      with
+        _ -> raise Unification_failure
+    else
+      raise Unification_failure
+
+  |Term.Fun(f, sous_termes), Term.Var v|Term.Var v, Term.Fun(f, sous_termes) ->
+    (match Hashtbl.find_opt Term.global_state v with
+    |None -> Term.bind v (Term.make f sous_termes)
+    |Some t -> unify t (Term.make f sous_termes)            
+    )
+
+  |Term.Var v1, Term.Var v2 ->
+    match Hashtbl.find_opt v1, Hashtbl.find_opt v2 with
+    |None, None -> Term.bind v1 (Term.var v2)
+    |None, Some t -> Term.bind v1 t
+    |Some t, None -> Term.bind v2 t
+    |Some t1', Some t2' -> unify t1' t2'
