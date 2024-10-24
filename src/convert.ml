3fb30f812701825cc2feb981199a514788ac8cb5
AdilOub <adil.oubninte.pro@gmail.com>
2024-10-24 12:14:50 +0200

    debut convert

--- src/convert.ml
+++ src/convert.ml
@@ -1,1 +1,11 @@
-let rules 
+let variable_table (*todo utiliser une hashtbl str -> int pour les variables*)
+
+
+let to_asci str = let rec aux str somme index = if index=(String.length str) then somme else
+  let code = Char.code str.[index] in aux str (somme+code) (index+1)
+in aux str 0 0;; 
+
+
+let rec convert_atom_t t = match t with
+  | App (str, tl) -> Term.Fun(str, convert_atom_t tl)
+  | Var v -> Term.var v
